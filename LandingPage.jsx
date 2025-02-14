5663adbe2762e712fa63f54e92a8438f311e993c
Dwarampudi S R G N V Sasank Reddy <sasankreddydwarampudi3@gmail.com>
2025-02-14 22:10:14 +0530

    Update LandingPage.jsx

--- LandingPage.jsx
+++ LandingPage.jsx
@@ -1,5 +1,6 @@
 // LandingPage.jsx
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './LandingPage.css'; // CSS file for styling
 
 const LandingPage = () => {
@@ -12,6 +13,10 @@ const LandingPage = () => {
         <p>
           Welcome to My Company! We specialize in providing the best solutions for your needs. Our mission is to deliver quality and excellence in every project we undertake.
         </p> {/* Paragraph about the company */}
+        {/* Get Started Button linking to the project listing page */}
+        <Link to="/project-listing" className="get-started-button">
+          Get Started
+        </Link>
       </main>
     </div>
   );
